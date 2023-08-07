98c719dccbfc5c9fa959b8191ae12d2e2ac1b161
andrei-kaliada <koleda05@gmail.com>
2023-08-07 08:44:51 +0200

    Created genre module.

---
+++ back/src/genre/genre.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+import { GenreController } from './genre.controller'
+import { GenreService } from './genre.service'
+
+@Module({
+  providers: [GenreService],
+  controllers: [GenreController]
+})
+export class GenreModule {}
